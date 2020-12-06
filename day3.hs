48ac214ee304ec1915d7bcee86c22d3e293402ab
Jacob Peyron <jacob.peyron@gmail.com>
2020-12-06 20:01:41 +0100

    Day 3 part 2

--- day3.hs
+++ day3.hs
@@ -3,16 +3,22 @@ import Common
 isTree :: Char -> Bool
 isTree c = c == '#'
 
-rowIsTree :: Int -> String -> Bool
-rowIsTree i s  = (isTree . (!! (i*3))) (cycle s)
+rowIsTree :: Int -> Int -> String -> Bool
+rowIsTree dx i s  = (isTree . (!! (i*dx))) (cycle s)
 
-tst :: [Int] -> Int
-tst x = sum x
+skip :: [a] -> [a]
+skip [n] = [n]
+skip (x:xs) = x:(skip (tail xs))
+
+descent :: [String] -> Int -> Int
+descent s dx = y where
+    y = length (filter (==True) treeRows)
+    treeRows = [rowIsTree dx (fromIntegral a) b | (a,b) <- enumerate s]
 
 solve :: [String] -> Int
 solve s = y where
-    y = length (filter (==True) treeRows)
-    treeRows = [rowIsTree (fromIntegral a) b | (a,b) <- enumerate s]
+    y = product ((descent (skip s) 1):simpl)
+    simpl = map (descent s) [1, 3, 5, 7]
 
 mini = id ["..##.......",
            "#...#...#..",
